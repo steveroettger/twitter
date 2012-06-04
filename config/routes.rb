f264f9c1b143fbd4ef4b294c8f7243b2362ab97f
Steve Roettger <steve@steveroettger.com>
2012-06-04 15:12:00 -0400

    Completed static pages w/ titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Twitter::Application.routes.draw do
- 
   get "static_pages/home"
-
   get "static_pages/help"
-
- get "static_pages/home"
- get "static_pages/home"
+  get "static_pages/about"
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the h1 'Twitter'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Twitter')
+    end
+    
+    it "should have the title 'Twitter'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Twitter | Welcome")
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text =>'Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Twitter | Help")
+    end
+  end
+  
+  describe "About page" do
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Twitter | About Us")
+    end
+  end
+end
