5d78266a0a9152a17d35218fa86f459cabb8fd93
Steve Roettger <steve@steveroettger.com>
2012-06-04 21:16:49 -0400

    completed refactoring of static_page spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,63 +1,30 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  subject { page }
+  
   describe "Home page" do
     before { visit root_path } 
-
-    it "should have the h1 'Twitter'" do
-      page.should have_selector('h1', text: 'Welcome to Twitter!')
-    end
-
-    it "should have the base title" do
-      page.should have_selector('title',
-                        text: "Twitter")
-    end
-
-    it "should have a custom page title" do
-      page.should have_selector('title', text: '| Welcome!')
-    end
+    it { should have_selector('h1', text: 'Welcome to Twitter!') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector('title', text: '| Welcome!') }
   end
 
   describe "Help page" do
-    
     before { visit help_path}
-
-    it "should have the h1 'Help'" do
-      page.should have_selector('h1', text: 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      page.should have_selector('title',
-                        text: "Twitter | Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
-    
     before { visit about_path }
-
-    it "should have the h1 'About'" do
-      page.should have_selector('h1', text: 'About')
-    end
-
-    it "should have the title 'About Us'" do
-      page.should have_selector('title',
-                    text: "Twitter | About")
-    end
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
   end
 
   describe "Contact page" do
-    
     before { visit contact_path }
-
-    it "should have the h1 'Contact'" do
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      page.should have_selector('title',
-                    text: "Twitter | Contact")
-    end
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
