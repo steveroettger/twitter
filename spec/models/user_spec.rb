f8ad1a9662de7681e133e7088ec4d44efea60f77
Steve Roettger <steve@steveroettger.com>
2012-06-06 10:01:18 -0400

    fixed failing email duplication test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,10 +66,20 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      user_with_same_email = @user.email.upcase
+      user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
     it { should_not be_valid }
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@eExAMPle.CoM" }
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
   
   describe "when password is not present" do
